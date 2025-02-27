576785da6baa3e4223fe779e67e71b16ed2e649b
henslea7 <henslea7@mail.uc.edu>
2025-02-27 15:22:09 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Task 1: ");
+        for (int i = 0; i <= 30; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
